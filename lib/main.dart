318237193becaaa1d9542c09d31f9c0b9474331e
unknown <kadivalirshad16@gmail.com>
2022-09-12 12:54:57 +0530

    fisrt commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'dart:ffi';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
